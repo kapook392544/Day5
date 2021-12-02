64a959b32ce94169836cad5a91dcc37e27461582
kapook392544 <64309010027@chontech.ac.th>
2021-12-02 10:24:56 +0700

    day5

--- Day12/login.php
+++ Day12/login.php
@@ -3,9 +3,9 @@ session_start();
 $user=$_POST['uname'];
 $pass=$_POST['psw'];
 
-if ($user =='Noey' and $pass =='392544'){
+if ($user =='kapuk' and $pass =='392544'){
     $_SESSION['user']=$user;
-    $_SESSION['name'] = "กฤติยาณี จันทะบัณฑิต";
+    $_SESSION['name'] = "ภัทรวดี นะราชรัมย์";
   header("location:page1.php");
 }else{
   header("location:index.php");
